e6cb0f69be6bf7d900cb1569f5c699d7a44d4586
Slobodan Vujovic <slobodan.vujovic@zuehlke.com>
2023-09-18 06:31:05 +0200

    Crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,6 +1,33 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+async function crawlUrl(currentUrl) {
+  console.log(`activly crawling of: ${currentUrl}`);
+
+  try {
+    const resp = await fetch(currentUrl);
+
+    if (resp.status > 399) {
+      console.log(
+        `Error in fetch with status code: ${resp.status}, on page: ${currentUrl}`
+      );
+      return;
+    }
+
+    const contentTypeHeader = resp.headers.get("content-type");
+    if (!contentTypeHeader.includes("text/html")) {
+      console.log(
+        `Not HTML response, content type: ${contentTypeHeader}, on page: ${currentUrl}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (error) {
+    console.log(`Error: ${error.message}, while crawling URL: ${currentUrl}`);
+  }
+}
+
 function getUrlsFromHtml(htmlPage, baseUrl) {
   const urls = [];
   const page = new JSDOM(htmlPage);
@@ -45,4 +72,5 @@ function normalizeUrl(inputUrl) {
 module.exports = {
   normalizeUrl,
   getUrlsFromHtml,
+  crawlUrl,
 };
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const { crawlUrl } = require("./crawl.js");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("Crawl URL missing");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("Too many arguments");
+    process.exit(1);
+  }
+
+  const currentUrl = process.argv[2];
+  console.log(`Start crawling URL ${currentUrl}`);
+  crawlUrl(process.argv[2]);
+}
+
+main();
